9fffc9466c7ac580b06ee90ba395bc14cb3a00d3
ganesh.pai <ganeshpaih24@gmail.com>
2024-07-24 10:39:47 +0530

    added task 3 solutions

--- Team-2024/studentRecord.py
+++ Team-2024/studentRecord.py
@@ -17,3 +17,22 @@ class studentScore:
                         "science": row['science']
                     }
             return f"No records found for rollno {Rollno}"
+
+    def mainMenu(self):
+        while True:
+            print("Main Menu")
+            print("1. Retrieve Student Score")
+            # print("2. Store Student Score")
+            print("2. Exit")
+            choice = input("Enter your choice: ").strip()
+
+            if choice == '1':
+                rollno = int(input("Enter RollNo: ").strip())
+                print(self.RetrieveStudentScore(rollno))
+            elif choice == '2':
+                # self.StoreStudentScore()
+            # elif choice == '3':
+                print("Exiting...")
+                break
+            else:
+                print("Invalid choice. Please try again.")
